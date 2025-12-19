c096a504aaee451915841c449fa2e01b749e4242
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-19 11:31:07 +0700

    Refactor Cylinder class to use length instead of height

--- CS/cs_oopwork07/geometry/Cylinder.cs
+++ CS/cs_oopwork07/geometry/Cylinder.cs
@@ -2,31 +2,31 @@ namespace cs_oopwork07;
     public class Cylinder
     {
         private double radius;
-        private double height;
+        private double length;
 
         public double Radius { get => radius; set => radius = value; }
-        public double Height { get => height; set => height = value; }
+        public double Length { get => length; set => length = value; }
 
-        public Cylinder(double radius, double height)
+        public Cylinder(double radius, double length)
         {
             this.radius = radius;
-            this.height = height;
+            this.length = length;
         }
 
         public Cylinder():this(1.0, 1.0){}
 
         public double area()
         {
-            return Math.PI * Math.Pow(radius, 2);
+            return 2 * Math.PI * radius * (radius + length);
         }
 
         public double volume()
         {
-            return Math.PI * Math.Pow(radius, 2) * height;
+            return area() * length;
         }
 
         public void CyInfo()
         {
-            System.Console.WriteLine($"Cylinder: Radius = {Radius:N2}, Height = {Height:N2}, Area = {area():N2}, Volume = {volume():N2}");
+            System.Console.WriteLine($"Cylinder: Radius = {Radius:N2}, Length = {Length:N2}, Area = {area():N2}, Volume = {volume():N2}");
         }
     }
