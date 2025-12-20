88120640085d4b255895caec84359054d1701d5d
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-20 14:06:30 +0700

    Add instances for Circle, Cylinder, and Rectangle

--- CS/cs_oopwork07/Program.cs
+++ CS/cs_oopwork07/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace cs_oopwork07;
+namespace cs_oopwork07;
 
 class Program
 {
@@ -8,17 +8,23 @@ class Program
         cir1.cirInfo();
         cir1.Radius = 5.0;
         cir1.cirInfo();
+        var cir2 = new Circle { Radius = 6.0 };
+        cir2.cirInfo();
 
         var cy1 = new Cylinder();
         cy1.CyInfo();
         cy1.Radius = 3.0;
         cy1.Length = 6.0;
         cy1.CyInfo();
+        var cy2 = new Cylinder { Radius = 4.0, Length = 4.0 };
+        cy2.CyInfo();
 
         var rec1 = new Rectangle();
         rec1.RecInfo();
         rec1.Width = 5.0;
         rec1.Height = 6.0;
         rec1.RecInfo();
+        var rec2 = new Rectangle { Width = 7.0, Height = 5.0 };
+        rec2.RecInfo();
     }
 }
