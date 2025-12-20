dc5da3215a831834bc751af95e5133b4f616791a
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-20 14:07:18 +0700

    Simplify Rectangle class properties and constructor
    Refactored Rectangle class to simplify properties and constructors.

--- CS/cs_oopwork07/geometry/Rectangle.cs
+++ CS/cs_oopwork07/geometry/Rectangle.cs
@@ -1,23 +1,13 @@
 namespace cs_oopwork07;
     public class Rectangle
     {
-        private double width;
-        private double height;
 
-        public double Width { get => width; set => width = value; }
-        public double Height { get => height; set => height = value; }
-
-        public Rectangle(): this(1.0, 1.0) {}
-
-        public Rectangle(double width, double height)
-        {
-            this.width = width;
-            this.height = height;
-        }
+        public double Width { get; set; } = 1.0;
+        public double Height { get; set; } = 1.0;
 
         public double area()
         {
-            return width * Height;
+            return Width * Height;
         }
 
         public void RecInfo()
