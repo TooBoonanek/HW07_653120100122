efd9f89d1909ef3b01b10d40a03f2f4b56d16708
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-20 15:49:42 +0700

    Refactor Rectangle class to use expression-bodied members

--- CS/cs_oopwork07/geometry/Rectangle.cs
+++ CS/cs_oopwork07/geometry/Rectangle.cs
@@ -5,13 +5,7 @@ namespace cs_oopwork07;
         public double Width { get; set; } = 1.0;
         public double Height { get; set; } = 1.0;
 
-        public double area()
-        {
-            return Width * Height;
-        }
+        public double area => Width * Height;
 
-        public void RecInfo()
-        {
-            System.Console.WriteLine($"Rectangle: Width = {Width:N2}, Height = {Height:N2}, Area = {area():N2}");
-        }
+        public void RecInfo => $"Rectangle: Width = {Width:N2}, Height = {Height:N2}, Area = {area():N2}";
     }
