12973ced935066282f878ded58883874ee4c72b0
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-20 15:46:40 +0700

    Update Cylinder.cs

--- CS/cs_oopwork07/geometry/Cylinder.cs
+++ CS/cs_oopwork07/geometry/Cylinder.cs
@@ -6,18 +6,9 @@ namespace cs_oopwork07;
         public double Length { get; set; } = 1.0;
 
 
-        public double area()
-        {
-            return 2 * Math.PI * Radius * (Radius + Length);
-        }
+        public double area => return 2 * Math.PI * Radius * (Radius + Length);
 
-        public double volume()
-        {
-            return area() * Length;
-        }
+        public double volume => return area() * Length;
 
-        public void CyInfo()
-        {
-            System.Console.WriteLine($"Cylinder: Radius = {Radius:N2}, Length = {Length:N2}, Area = {area():N2}, Volume = {volume():N2}");
-        }
+        public void CyInfo => $"Cylinder: Radius = {Radius:N2}, Length = {Length:N2}, Area = {area:N2}, Volume = {volume:N2}";
     }
