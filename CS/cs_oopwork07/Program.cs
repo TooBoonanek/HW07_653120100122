942fefbeeaf9de1aff8301d0319889d5f36429d2
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-20 15:52:14 +0700

    Update Program.cs

--- CS/cs_oopwork07/Program.cs
+++ CS/cs_oopwork07/Program.cs
@@ -4,27 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        var cir1 = new Circle();
-        cir1.cirInfo();
+        var cir1 = new Circle;
+        Console.WriteLine(cir1.cirInfo);
         cir1.Radius = 5.0;
-        cir1.cirInfo();
+        Console.WriteLine(cir1.cirInfo);
         var cir2 = new Circle { Radius = 6.0 };
-        cir2.cirInfo();
+        Console.WriteLine(cir2.cirInfo);
 
-        var cy1 = new Cylinder();
-        cy1.CyInfo();
+        var cy1 = new Cylinder;
+        Console.WriteLine(cy1.CyInfo);
         cy1.Radius = 3.0;
         cy1.Length = 6.0;
-        cy1.CyInfo();
+        Console.WriteLine(cy1.CyInfo);
         var cy2 = new Cylinder { Radius = 4.0, Length = 4.0 };
-        cy2.CyInfo();
+        Console.WriteLine(cy2.CyInfo);
 
-        var rec1 = new Rectangle();
-        rec1.RecInfo();
+        var rec1 = new Rectangle;
+        Console.WriteLine(rec1.RecInfo);
         rec1.Width = 5.0;
         rec1.Height = 6.0;
-        rec1.RecInfo();
+        Console.WriteLine(rec1.RecInfo);
         var rec2 = new Rectangle { Width = 7.0, Height = 5.0 };
-        rec2.RecInfo();
+        Console.WriteLine(rec2.RecInfo);
     }
 }
