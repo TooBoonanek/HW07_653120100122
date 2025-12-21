049adc51affe9077d9ead9cf3442b988a62e756a
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-21 15:45:07 +0700

    Update Circle.cs

--- CS/cs_oopwork07/geometry/Circle.cs
+++ CS/cs_oopwork07/geometry/Circle.cs
@@ -5,9 +5,5 @@ namespace cs_oopwork07;
         
         public double area => Math.PI * Math.Pow(Radius, 2);
 
-        public double diamater => 2 * Radius;
-
-        public double circumference => 2 * Math.PI * Radius;
-
-        public string cirInfo => $"Circle: Radius = {Radius:N2}, Area = {area:N2}, Diameter = {diamater:N2}, Circumference = {circumference:N2}";
+        public string cirInfo => $"Circle: Radius = {Radius:N2}, Area = {area:N2}";
     }
